e7cbe106679a49b347ae05f52712cb24d2d76b56
Diksha Dhakate <diksha.dhakate6@gmail.com>
2020-04-08 19:10:11 +0530

    added gems for bootstrap themes and made modification in manifest.js file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
-  def index
-  end
-  
+	skip_before_action :authenticate_user!, only: [:index] #this is use because it will skip user login for root page
+
+	def index
+	end
+
 end
